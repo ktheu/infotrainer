10e92a5659ca1b6cb2549ec4f8347c487cbc96e4
Hypercookie <hypercookie@gmx.de>
2018-03-09 08:24:07 +0100

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -1,12 +1,16 @@
 def Collatz_ui():
     x = int(input("Bitte eine Zahl eingeben:" ))
     z=0
+    print("Folge der Zahlen:",end=" ")
     while x!= 1:
         if x%2 == 0:
             x=x//2
         else:
             x=3 * x+1
+        print(x,end=" ")
         z=z+1
+    print("")
+    print("Ergebnis: ",end="")
     print(z)
 
 if __name__ == '__main__':
