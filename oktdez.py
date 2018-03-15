165bd263b85e5a52f7c2562a874f79b9724fb32e
ktheu <khtheuer@t-online.de>
2018-03-15 09:23:35 +0100

    input eingefügt

--- oktdez.py
+++ oktdez.py
@@ -1,6 +1,5 @@
 import random
 
-
 def berechnung():
     summe = 0
     multiplikator = 1
@@ -9,9 +8,9 @@ def berechnung():
     for z in range(0, 5): #Zahl wird erstellt
         i = i + str(random.randint(0, 7))
 
-
     i_rev = str(i)
     print("Wandle die Oktalzahl in eine Dezimalzahl um: ",i_rev)
+    input
 
     i_rev = i_rev[::-1] # Zahl wird umgedreht
 
@@ -19,16 +18,7 @@ def berechnung():
         summe = summe + int(i_rev[x]) * multiplikator
         multiplikator *= 8
 
-
-
     print("Die Dezimalzahl ist ", i_rev[0], "*1 + ",i_rev[1], "*8 + ",i_rev[2], "*64 + ",i_rev[3], "*512 ",i_rev[4], "*4096 = ", summe)
-
-
 
 def oktdez_ui():
        berechnung()
-
-
-
-
-
