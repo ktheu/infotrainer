29a78e35a5b6d64c2ded6240bd026e400705985a
RaduM25 <36982152+RaduM25@users.noreply.github.com>
2018-03-19 12:18:24 +0100

    Erste Ziffer nicht 0

--- oktdez.py
+++ oktdez.py
@@ -6,7 +6,10 @@ def berechnung():
     i = "" #Zahl die später erstellt wird
 
     for z in range(0, 5): #Zahl wird erstellt
-        i = i + str(random.randint(0, 7))
+        if z > 0:
+            i = i + str(random.randint(0, 7))
+        else:
+            i = i + str(random.randint(1, 7))
     i_rev = str(i)
     i_rev = i_rev[::-1] # Zahl wird umgedreht
 
@@ -20,6 +23,5 @@ def berechnung():
 def oktdez_ui():
     i_rev, summe = berechnung()
     print("Wandle die Oktalzahl in eine Dezimalzahl um: ",i_rev[::-1])
-    output = "Die Dezimalzahl ist %s *1 + %s *8 + %s *64 + %s *512 %s *4096 = %s " %(i_rev[0],i_rev[1],i_rev[2],i_rev[3],i_rev[4],summe)
+    output = "Die Dezimalzahl ist %s *1 + %s *8 + %s *64 + %s *512 + %s *4096 = %s " %(i_rev[0],i_rev[1],i_rev[2],i_rev[3],i_rev[4],summe)
     print(output)
-
